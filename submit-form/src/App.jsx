454d9419862baf718896b56dfa927f6b522311cb
HarCass <harry.jc.cass@gmail.com>
2023-06-26 16:03:25 +0100

    basic setup and implementation for submit form

---
+++ submit-form/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import InfoForm from './InfoForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Submit Form</h1>
+      <InfoForm/>
+    </div>
+  );
+}
+
+export default App;
